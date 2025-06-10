8d2a601b8e1476bb70b588978991c200e5384dc5
kettle17 <trainee.callum.peters@sigmalabs.co.uk>
2025-06-10 17:14:27 +0100

    Added pylint exclusion, fixed docstrings

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,24 +1,25 @@
+# pylint: skip-file
 """Script for the extract part of the ETL."""
 
 import time
 
 
 def get_api_request(start_date: int, base_api_url: str) -> dict:
-    """Function that gets objects from a given API URL call.
+    """Returns objects from a given API URL call.
     Separated from fetch_api_data to allow mocking."""
     start_date -= 200  # subtract 200 from current time so report is generated
     pass
 
 
 def fetch_api_data(start_date: int, base_api_url: str) -> dict:
-    """Function that connects to the API and returns the data.
+    """Connects to the API and returns the data.
     Start date is a seconds from epoch int that gets called in the API under start_date.
     This is the report that gets called every 2 minutes."""
     pass
 
 
 def save_api_data(api_data: dict, file_path: str) -> bool:
-    """Function that takes the JSON and converts it into a base CSV file.
+    """Takes the dictionary contents and converts it into a base CSV file.
     Returns true or false based on if the API was able to be saved locally."""
     pass
 
@@ -26,12 +27,12 @@ def save_api_data(api_data: dict, file_path: str) -> bool:
 def run_extract(file_path: str,
                 curr_time: int = int(time.time()),
                 base_api_url: str = "https://bandcamp.com/api/salesfeed/1/get?start_date="):
-    """Function that runs the above three all in one."""
+    """Runs all of the script's contents together."""
     pass
 
 
 def get_time_offset(curr_time: int = int(time.time()), offset: int = 200) -> int:
-    """Get current time in epoch format, minus time offset"""
+    """Get current time in epoch format, minus time offset."""
     return curr_time - offset
 
 
