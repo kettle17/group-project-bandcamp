5b7a4dbfd5ab8c929a82b2192c7c75e3140268ee
zertidana <94858570+zertidana@users.noreply.github.com>
2025-06-10 17:56:19 +0100

    Updates to func signatures

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -9,18 +9,13 @@ def get_required_columns(df: pd.DataFrame) -> pd.DataFrame:
     pass
 
 
-def check_valid_sale(df: pd.DataFrame) -> pd.DataFrame:
+def check_valid_sale(df: pd.DataFrame) -> bool:
     """Checks that the sale is valid using amount paid > 0."""
     pass
 
 
 def clean_urls(df: pd.DataFrame) -> pd.DataFrame:
-    """Cleans the urls to be used in the dashboard."""
-    pass
-
-
-def get_daily_report(df: pd.DataFrame) -> None:
-    """Returns what is required from the data to be included in the report."""
+    """Returns a pandas dataframe of cleaned urls."""
     pass
 
 
