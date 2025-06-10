d8d28c34e533d18daaafa54deb2d2f22b27cd9d1
kettle17 <trainee.callum.peters@sigmalabs.co.uk>
2025-06-10 17:27:56 +0100

    Renaming test functions

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -7,7 +7,7 @@ import os
 from extract import get_api_request, fetch_api_data, export_api_data_to_csv, run_extract, get_time_offset
 
 
-def test_if_extract_exists():
+def test_if_extract_exists_should_exist():
     """Basic beginner test. If this test can't run, neither can the rest."""
     try:
         assert os.path.exists('pipeline/extract.py')
@@ -15,12 +15,12 @@ def test_if_extract_exists():
         assert os.path.exists('extract.py')
 
 
-def test_offset_5000():
+def tests_get_time_offset_passing_5000_should_return_4800():
     """Tests if the offset is working."""
     assert get_time_offset(5000) == 4800
 
 
-def test_offset_string():
+def tests_get_time_offset_raises_errors():
     """Tests if the function errors as expected."""
     with pytest.raises(TypeError):
         get_time_offset('crazy')
