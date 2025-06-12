4ae26949307f5ded0cef4ed7e6ed611ea5f6e92e
Najma <trainee.najma.hassan@sigmalabs.co.uk>
2025-06-12 14:51:04 +0100

    added logging and further tests for invalid inputs

--- pipeline/test_web_scraper.py
+++ pipeline/test_web_scraper.py
@@ -1,9 +1,14 @@
 # pylint: skip-file
 
 """Test file for web scraper functions."""
+from unittest.mock import Mock, patch
 import pytest
 from bs4 import BeautifulSoup
-from web_scraper import get_release_date, filter_tags, get_genres
+from web_scraper import get_relevant_html, get_release_date, filter_tags, get_genres, get_release_date_and_genres
+from utilities import set_logger, get_logger
+
+set_logger()
+logger = get_logger()
 
 
 @pytest.mark.parametrize("tags, expected_genres",
@@ -18,7 +23,21 @@ def test_get_filter_tags_returns_correct_tags(tags, expected_genres):
     """Tests that get_filter_tags filters out tags starting 
     with a capital letter."""
 
-    assert filter_tags(tags) == expected_genres
+    assert filter_tags(tags, logger) == expected_genres
+
+
+@pytest.mark.parametrize("invalid_tags",
+                         (
+                             (123),
+                             (None),
+                             ("techno, electronic, pop"),
+                             ("techno")
+                         ))
+def test_get_filter_tags_raises_error_if_invalid_tags_type(invalid_tags):
+    """Tests that get_filter_tags raises an error when an invalid 
+     tags type is provided."""
+    with pytest.raises(TypeError):
+        filter_tags(invalid_tags, logger)
 
 
 @pytest.mark.parametrize("test_html, expected_date",
@@ -36,7 +55,40 @@ def test_get_release_date_returns_only_release_date(test_html, expected_date):
       present. """
 
     soup = BeautifulSoup(test_html, "html.parser")
-    assert get_release_date(soup) == expected_date
+    assert get_release_date(soup, logger) == expected_date
+
+
+@pytest.mark.parametrize("test_html, expected_result",
+                         (
+                             ("<div class='tralbumData tralbum-credits'"
+                              "<br>\n written by C.Hammer",
+                              None),
+                             ("<div class='tralbumData tralbum-credits'"
+                              " <br>\n written by C.Hammer"
+                              " <br> produced by DayBehaviour", None)
+                         ))
+def test_get_release_date_when_no_release_date_found(test_html, expected_result):
+    """Tests that the get_release_date function return None if no release date
+     found for given item. """
+
+    soup = BeautifulSoup(test_html, "html.parser")
+    assert get_release_date(soup, logger) == expected_result
+
+
+@pytest.mark.parametrize("test_input",
+                         (
+                             ("<div class='tralbumData tralbum-credits'> released June 11, 2025 "
+                              "<br>\n written by C.Hammer"),
+                             ("<div class='tralbumData tralbum-credits'> released July 11, 2025"
+                              " <br>\n written by C.Hammer"
+                              " <br> produced by DayBehaviour"),
+                             (["Not a BeautifulSoup Tag"])
+                         ))
+def test_get_release_date_if_invalid_input_given(test_input):
+    """Tests that the get_release_date function raises an error if the
+     input provided is not a BeautifulSoup Tag. """
+    with pytest.raises(TypeError):
+        get_release_date(test_input, logger)
 
 
 @pytest.mark.parametrize("test_html, expected_genres",
@@ -55,4 +107,45 @@ def test_get_genres_returns_only_genres(test_html, expected_genres):
       present about country. """
 
     soup = BeautifulSoup(test_html, "html.parser")
-    assert get_genres(soup) == expected_genres
+    assert get_genres(soup, logger) == expected_genres
+
+
+@pytest.mark.parametrize("test_html, expected_result",
+                         (
+                             ("<a class='tag'>Sweden</a><a class='tag'>Germany</a>",
+                              []),
+                             ("<a class='tag'>Argentina</a><a class='tag'>Sweden</a>",
+                              []),
+                             ("<a class='tag'>Not a genre</a> \n <a class='tag'>Germany</a>",
+                              []),
+                         ))
+def test_get_genres_when_no_genres_found(test_html, expected_result):
+    """Tests that the get_genres function returns an empty list when no
+     genres were found."""
+
+    soup = BeautifulSoup(test_html, "html.parser")
+    assert get_genres(soup, logger) == expected_result
+
+
+@patch("web_scraper.requests.get")
+def test_get_release_date_and_genres_returns_expected_dict(mock_get_request):
+    """Tests that the get_release_date_and_genres functions returns the expected 
+    dict containing release date and genres."""
+
+    test_html = """
+    <div id='pgBd'>
+    <div class='tralbumData tralbum-credits'>
+    released June 11, 2024 \n <br>by Artist
+    </div>
+    <a class='tag'>electronic</a>
+    <a class='tag'>Germany</a>
+    <a class='tag'>pop</a>
+    </div>"""
+    mock_response = Mock()
+    mock_response.content = test_html
+    mock_get_request.return_value = mock_response
+
+    result = get_release_date_and_genres("http://test.com", logger)
+
+    assert result["release_date"] == "released June 11, 2024"
+    assert result["genres"] == ["electronic", "pop"]
--- pipeline/web_scraper.py
+++ pipeline/web_scraper.py
@@ -1,31 +1,53 @@
 """Script for the web scraper part of ETL."""
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+
+from utilities import set_logger, get_logger
 
 
-def filter_tags(tags: list[str]) -> list[str]:
+def filter_tags(tags: list[str], logger) -> list[str]:
     """Filters the tags associated with the given item to return genres
-    and not country."""
+    and not country. Tags that start with lowercase letter are considered
+    genres."""
 
+    if not isinstance(tags, list):
+        logger.error("Error: Unexpected type. "
+                     " This functions expects a list.")
+        raise TypeError("Input to filter tags must be a list.")
     genres = []
     for tag in tags:
         if tag[0].islower():
+            logger.info(f"Tag is a genre: {tag}")
             genres.append(tag)
         else:
-            continue
+            logger.info(f"Tag is not a genre, most likely a country: {tag} ")
     return genres
 
 
-def get_relevant_html(url: str):
+def get_relevant_html(url: str, logger) -> Tag:
     """Returns the html element containing details on release date and tags."""
+
+    logger.info(f"Retrieving page to be scraped from '{url}'")
     page = requests.get(url, timeout=5)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(id="pgBd")
+
+    if not results:
+        logger.error("Could not find element with id 'pgBd in HTML.")
+        raise ValueError("Could not find element with id 'pgBd in HTML.")
+
+    logger.info("Relevant HTML content retrieved successfully.")
     return results
 
 
-def get_release_date(results) -> str:
+def get_release_date(results: Tag, logger) -> str:
     """Returns the release date for a given item."""
+
+    if not isinstance(results, Tag):
+        logger.error("Unexpected input. Results is not a BeautifulSoup Tag.")
+        raise TypeError(
+            "Unexpected input. Results is not a BeautifulSoup Tag.")
+
     release_date = results.find(
         "div", class_="tralbumData tralbum-credits")
 
@@ -34,23 +56,48 @@ def get_release_date(results) -> str:
         if "released" in line.strip():
             return line.strip()
 
+    logger.debug("No release date found in HTML content.")
 
-def get_genres(results) -> list[str]:
+
+def get_genres(results: Tag, logger) -> list[str]:
     """Returns a list of genres associated with the item."""
+    if not isinstance(results, Tag):
+        logger.error("Unexpected input. Results is not a BeautifulSoup Tag.")
+        raise TypeError(
+            "Unexpected input. Results is not a BeautifulSoup Tag.")
+
     tags_list = []
     tags = results.find_all("a", class_="tag")
 
+    if not tags:
+        logger.error("No tags found with the class 'tag' in HTML.")
     for tag in tags:
         tags_list.append(tag.text.strip())
 
-    genres = filter_tags(tags_list)
+    genres = filter_tags(tags_list, logger)
 
-    genres = filter_tags(tags_list)
+    if not genres:
+        logger.info("No genre found given item.")
+
     return genres
 
 
+def get_release_date_and_genres(url: str, logger) -> dict:
+    """Scrapes the given web page and returns the release date and genres."""
+
+    details = {}
+    html = get_relevant_html(url, logger)
+    item_release_date = get_release_date(html, logger)
+    item_genres = get_genres(html, logger)
+
+    details["release_date"] = item_release_date
+    details["genres"] = item_genres
+
+    return details
+
+
 if __name__ == "__main__":
-    html = get_relevant_html(
-        "https://daybehavior.bandcamp.com/track/silver-song")
-    item_release_date = get_release_date(html)
-    item_genres = get_genres(html)
+    set_logger()
+    scraper_logger = get_logger()
+    release_date_and_genres = get_release_date_and_genres(
+        "https://daybehavior.bandcamp.com/track/silver-song", scraper_logger)
